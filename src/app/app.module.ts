d987f9088a691204076ce26c3b27aa8a8195f080
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2019-01-20 20:57:01 -0600

    feat: Ajuste de dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+// componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FormularioRegistroAlumnoComponent } from './components/formulario-registro-alumno/formulario-registro-alumno.component';
 import { CargaDocumentosComponent } from './components/carga-documentos/carga-documentos.component';
 import { HomeInscripcionesComponent } from './components/home-inscripciones/home-inscripciones.component';
-
-// modulos
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InscripcionWizardComponent } from './components/inscripcion-wizard/inscripcion-wizard.component';
 import { DetalleAlumnoComponent } from './components/detalle-alumno/detalle-alumno.component';
 import { LoginComponent } from './components/login/login.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContratoComponent } from './components/contrato/contrato.component';
+import { ConfirmacionComponent } from './components/confirmacion/confirmacion.component';
+import { SuccessWizardComponent } from './components/success-wizard/success-wizard.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// modulos de angular
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import { Subject } from 'rxjs';
 
 // servicios
 import { MenuService } from './services/menu.service';
@@ -25,37 +31,17 @@ import { AlumnoService } from './services/alumno.service';
 import { FileService } from './services/file.service';
 import { WizardService } from './services/wizard.service';
 
+// Angular material
+import { MaterialModule } from './material';
 
-// import Table with paginator
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-
-
+// Angular cdk
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
 
-
-// Librerias
+// ng2
 import { FileSelectDirective } from 'ng2-file-upload';
-
-import { InscripcionWizardComponent } from './components/inscripcion-wizard/inscripcion-wizard.component';
-
-// Material
-import { MaterialModule } from './material';
-import { SuccessWizardComponent } from './components/success-wizard/success-wizard.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-import { ContratoComponent } from './components/contrato/contrato.component';
-
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { ConfirmacionComponent } from './components/confirmacion/confirmacion.component';
-
-
 
 const appRoutes: Routes = [
   {path: 'formularioDatos', component: FormularioRegistroAlumnoComponent},
@@ -67,37 +53,20 @@ const appRoutes: Routes = [
   {path: 'wizard', component: InscripcionWizardComponent},
   {path: 'wizard-success', component: SuccessWizardComponent},
   {path: 'contrato', component: ContratoComponent}
-
-    ];
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    FormularioRegistroAlumnoComponent,
-    CargaDocumentosComponent,
-    HomeInscripcionesComponent,
-    DetalleAlumnoComponent,
-    LoginComponent,
-    PerfilComponent,
-    FileSelectDirective,
-    InscripcionWizardComponent,
-    SuccessWizardComponent,
-    FooterComponent,
-    ContratoComponent,
-    ConfirmacionComponent
-
+    AppComponent, HeaderComponent, MenuComponent, FormularioRegistroAlumnoComponent,
+    CargaDocumentosComponent, HomeInscripcionesComponent, DetalleAlumnoComponent,
+    LoginComponent, PerfilComponent, FileSelectDirective, InscripcionWizardComponent,
+    SuccessWizardComponent, FooterComponent, ContratoComponent, ConfirmacionComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
     BrowserAnimationsModule,
     DragDropModule,
     ScrollingModule,
@@ -105,11 +74,10 @@ const appRoutes: Routes = [
     CdkTreeModule,
     ReactiveFormsModule,
     MaterialModule,
-    RouterModule.forRoot(appRoutes),
-    MatCheckboxModule
-
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [MenuService, AlumnoService, LoginService, FileService, WizardService],
+  providers: [MenuService, AlumnoService, LoginService, FileService,
+    WizardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,12 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule,
+  MatInputModule} from '@angular/material';
 import {MatStepperModule} from '@angular/material/stepper';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatStepperModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatStepperModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatStepperModule, MatTableModule,
+    MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatStepperModule, MatTableModule,
+    MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule { }
