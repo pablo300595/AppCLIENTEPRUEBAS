2b282bfa6d70294f84fe7227756ed6075c0bd6f5
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2019-06-29 01:30:02 -0500

    feat: Ajustes de convergencia

--- src/app/components/roles-carreras/roles-carreras.component.ts
+++ src/app/components/roles-carreras/roles-carreras.component.ts
@@ -20,47 +20,75 @@ export class RolesCarrerasComponent implements OnInit {
   currentUserToLoadCareer: any; // Stores WHOLE current user data
 
   testData: any[];
-  secretaries: Object[];
+  secretaries: any[];
   radioSelected: any;
   constructor(private messagesService: MessagesService, private secretariaService: SecretariaService,
-    private usuarioService: UsuarioService, private loginService: LoginService) { }
+    private usuarioService: UsuarioService, private loginService: LoginService) { 
+
+    }
 
   ngOnInit() {
     this.secretaries = [
-      {index: 0, secreName: 'Celia Estefania'},
-      {index: 1, secreName: 'Sofia Estrella'}
+      {index: 0, secreName: 'Celia Estefania', _id: '5cba1f8a2ebc2b0017e1b6ce', asignation: []},
+      {index: 1, secreName: 'Sofia Estrella',  _id: '5ced07121c9d440000db48ca', asignation: []}
     ];
     this.testData = [
-      {pos: 0, career: 'Ingeniería en Sistemas Computacionales', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca']},
-      {pos: 1, career: 'Maestría en Ciencias en Alimentos', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca']},
-      {pos: 2, career: 'Ingeniería Bioquímica', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca']}
+      {pos: 0, career: 'Ingeniería en Sistemas Computacionales', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 1, career: 'Maestría en Ciencias en Alimentos', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 2, career: 'Ingeniería Bioquímica', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 3, career: 'Arquitectura', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 4, career: 'Ingeniería Civil', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 5, career: 'Ingeniería Eléctrica', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 6, career: 'Ingeniería en Gestión Empresarial', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 7, career: 'Ingeniería Industrial', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 8, career: 'Ingeniería Mecatrónica', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 9, career: 'Ingeniería Química', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 10, career: 'Ingeniería en Tecnología de la Información y Comunicaciones',
+      _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 11, career: 'Licenciatura en Administración', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 12, career: 'Maestría en Tecnologías de la Información', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''},
+      {pos: 13, career: 'Doctorado en Ciencias en Alimentos', _id: ['5cba1f8a2ebc2b0017e1b6ce', '5ced07121c9d440000db48ca'],
+      selectedId: ''}
     ];
   }
 
   createAsignation(data, radioInfo) {
-    let newArray: any = new Array(this.testData[0]._id.length, this.testData.length + 1);
     const searchedID = data._id[radioInfo.value];
+    this.testData[data.pos].selectedId = searchedID;
+  }
 
-    newArray[0][0] = 'gege';
-    for (let i = 0,  j = 0; i < this.testData[0]._id.length ; i++) {
-      newArray[i][j] = 'gege';
+  updateCareerAsignation() {
+    // Clean asignation Array
+    for (let i = 0; i < this.secretaries.length; i++) {
+      this.secretaries[i].asignation = [];
     }
-
-    for (let i = 0; i < this.testData[0]._id.length; i++) {
+    // Generate asignation Array
+    for (let i = 0; i < this.secretaries.length; i++) {
       for (let j = 0; j < this.testData.length; j++) {
-        if (searchedID) {
-          console.log(searchedID);
-          newArray[i][j]  = '';
+        if (this.secretaries[i]._id === this.testData[j].selectedId) {
+          this.secretaries[i].asignation.push(this.testData[j].career);
         }
       }
     }
-    console.log(newArray);
-  }
-
-  updateCareerAsignation(_id) {
-    const newCareer = [];
-    this.secretariaService.updateSecretariaCareer(newCareer, _id).subscribe( res => {
-      this.messagesService.success('¡Asignación de carreras actualizada!');
-    });
+    setTimeout(() => {
+      for (let i = 0; i < this.secretaries.length; i++) {
+        this.secretariaService.updateSecretariaCareer(this.secretaries[i].asignation, this.secretaries[i]._id).subscribe( res => {
+          this.messagesService.success('¡Asignación de carreras actualizada!');
+        });
+      }
+    }, 500);
   }
 }
